bad503e69f913a736b99ee476a28dcd72de7857d
James Cook <mokus@deepbondi.net>
2012-04-06 13:46:17 -0400

    Include custom ffi_closure_alloc code
    libffi on Mac OS doesn't have ffi_closure_alloc (and the one that ships with GHC doesn't allocate them in executable segments), so build and link in closure.c from a modern libffi when building on Mac OS.
    
    This also allows us to build with the option enabling freeing of entry points (which will greatly simplify memory management of wrappers)

---
+++ include/hs_libffi_closure.h
@@ -0,0 +1,4 @@
+#include <ffi.h>
+
+void * hs_ffi_closure_alloc (size_t size, void **code);
+void   hs_ffi_closure_free (void *ptr);
--- src/Foreign/LibFFI/Experimental/Closure.hs
+++ src/Foreign/LibFFI/Experimental/Closure.hs
@@ -11,8 +11,10 @@ import Foreign.Storable
 newtype Closure = Closure (Ptr Closure)
 newtype Entry = Entry (FunPtr Entry) deriving (Eq, Ord, Show, Storable)
 
-foreign import ccall ffi_closure_alloc :: CSize -> Ptr Entry -> IO Closure
-foreign import ccall ffi_closure_free  :: Closure -> IO ()
+foreign import ccall "hs_ffi_closure_alloc"
+    ffi_closure_alloc :: CSize -> Ptr Entry -> IO Closure
+foreign import ccall "hs_ffi_closure_free"
+    ffi_closure_free :: Closure -> IO ()
 
 type FFI_Impl t a = CIF t -> Ptr (SigReturn t) -> Ptr (Ptr ()) -> Ptr a -> IO ()
 
